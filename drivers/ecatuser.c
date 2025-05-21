34260f19b29113a2e146bf6dbc9356d07fbd1525
CoreyLee <lixiangjun@up3dtech.com>
2025-05-21 13:30:16 +0800

    [fix] 修复了首次启动不顺滑问题

--- drivers/ecatuser.c
+++ drivers/ecatuser.c
@@ -18,8 +18,8 @@ uint64 ref_time_base = 0;
 uint64 sync_start_time = 0;
 int64 app_time_offset = 0;
 
-PDO_Output *outputs1;
-PDO_Input *inputs1;
+PDO_Output *outputs[5];
+PDO_Input *inputs[5];
 
 
 char IOmap[200];
@@ -38,13 +38,28 @@ int cmdpos_raw;
 #define DEBUG 1
 
 
+static void _dm9000_delay_ms(uint16_t nms)
+{
+    /**  tips:
+     * 这里主频480MHz rt_hw_us_delay() 大于1000就会卡死
+     * 这里把它限制在500us，大于的部分就拆分
+     * **/
+    rt_uint32_t us = nms * 1000;
+    while (us > 500)
+    {
+        rt_hw_us_delay(500);
+        us -= 500;
+    }
+    rt_hw_us_delay(us);
+}
+
 static int drive_write8(uint16 slave, uint16 index, uint8 subindex, uint8 value)
 {
     int wkc = 0;
     wkc = ec_SDOwrite(slave, index, subindex, FALSE, sizeof(value), &value, EC_TIMEOUTRXM);
     if(wkc <= 0)
     {
-        printf("write8 failed slave:%d index:0x%x subindex:%d value:0x%x wkc:%d\r\n", slave, index, subindex, value, wkc);
+        rt_kprintf("write8 failed slave:%d index:0x%x subindex:%d value:0x%x wkc:%d\r\n", slave, index, subindex, value, wkc);
     }
     return wkc;
 }
@@ -55,7 +70,7 @@ static int drive_write16(uint16 slave, uint16 index, uint8 subindex, uint16 valu
     wkc = ec_SDOwrite(slave, index, subindex, FALSE, sizeof(value), &value, EC_TIMEOUTRXM);
     if(wkc <= 0)
     {
-        printf("write16 failed slave:%d index:0x%x subindex:%d value:0x%x wkc:%d\r\n", slave, index, subindex, value, wkc);
+        rt_kprintf("write16 failed slave:%d index:0x%x subindex:%d value:0x%x wkc:%d\r\n", slave, index, subindex, value, wkc);
     }
     return wkc;
 }
@@ -66,7 +81,7 @@ static int drive_write32(uint16 slave, uint16 index, uint8 subindex, uint32 valu
     wkc = ec_SDOwrite(slave, index, subindex, FALSE, sizeof(value), &value, EC_TIMEOUTRXM);
     if(wkc <= 0)
     {
-        printf("write32 failed slave:%d index:0x%x subindex:%d value:0x%x wkc:%d\r\n", slave, index, subindex, value, wkc);
+        rt_kprintf("write32 failed slave:%d index:0x%x subindex:%d value:0x%x wkc:%d\r\n", slave, index, subindex, value, wkc);
     }
     return wkc;
 }
@@ -397,19 +412,22 @@ int Servosetup(uint16 slave)
 
 void ecat_init(void)
 {
+  static int frist_in = 1;
     int slc;
 //    int i,chk;
     int cnt = 1;
 		int expectedWKC;
-    
+
+ 
     /* initialise SOEM, bind socket to ifname */
 	if (ec_init("eth0"))
 	{
-		printf("ec_init succeeded.\r\n");
-         HAL_Delay(100);
+		rt_kprintf("ec_init succeeded.\r\n");
+         _dm9000_delay_ms(100);
+ _start:   
 		if ( ec_config_init(TRUE) > 0 )
 		{
-			printf("%d slaves found and configured.\r\n",ec_slavecount);
+			rt_kprintf("%d slaves found and configured.\r\n",ec_slavecount);
             
 			if ( ec_slavecount >= 1 ) 
 			{
@@ -418,28 +436,38 @@ void ecat_init(void)
                      
                     for(slc = 1; slc <= ec_slavecount; slc++)
 										{
-											printf("Found %s at position %d\n", ec_slave[slc].name, slc);
+											rt_kprintf("Found %s at position %d\n", ec_slave[slc].name, slc);
 											ec_slave[slc].PO2SOconfig = &Servosetup;
 										}
                  }
             }
             
-            ec_configdc();//DC???????
+           
             ec_config_map(&IOmap);
-            ec_dcsync0(1, TRUE, SYNC0TIME, 250000); // SYNC0 on slave 1
-//						ec_dcsync0(2, TRUE, SYNC0TIME, 0); // SYNC0 on slave 2
-						
-						printf("Slaves mapped, state to SAFE_OP.\n \r");	
+            ec_configdc();//DC???????
+            for (int i = 1; i <= ec_slavecount; i++) {
+                if (ec_slave[i].hasdc) { 
+                    ec_dcsync0(i, TRUE, SYNC0TIME, 250000); 
+                }
+            }
+            _dm9000_delay_ms(500);
+            if(frist_in)
+            {
+              frist_in = 0;
+              goto _start;
+            }
+
+						rt_kprintf("Slaves mapped, state to SAFE_OP.\n \r");	
 						/* wait for all slaves to reach SAFE_OP state */
 			      ec_statecheck(0, EC_STATE_SAFE_OP,  EC_TIMEOUTSTATE);
 						/* read indevidual slave state and store in ec_slave[] */
 						ec_readstate();
 						
-						printf("Slave 0 State=0x%04x\r\n",ec_slave[0].state);
-						printf("Slave 1 State=0x%04x\r\n",ec_slave[1].state);			
+						rt_kprintf("Slave 0 State=0x%04x\r\n",ec_slave[0].state);
+						rt_kprintf("Slave 1 State=0x%04x\r\n",ec_slave[1].state);			
 						
 						for(cnt = 1; cnt <= ec_slavecount ; cnt++){
-						printf("Slave:%d Name:%s Output size:%3dbits Input size:%3dbits State:%2d delay:%d.%d\n \r",
+						rt_kprintf("Slave:%d Name:%s Output size:%3dbits Input size:%3dbits State:%2d delay:%d.%d\n \r",
 							cnt, ec_slave[cnt].name, ec_slave[cnt].Obits, ec_slave[cnt].Ibits,
 							ec_slave[cnt].state, (int)ec_slave[cnt].pdelay, ec_slave[cnt].hasdc);
 						}
@@ -452,12 +480,12 @@ void ecat_init(void)
 						if ((iloop == 0) && (ec_slave[0].Ibits > 0)) iloop = 1;
 						if (iloop > 30) iloop = 30;
 			
-						printf("oloop:%d iloop:%d\n\r",oloop,iloop);
+						rt_kprintf("oloop:%d iloop:%d\n\r",oloop,iloop);
 						
-						printf("segments : %d : %d %d %d %d\n \r",ec_group[0].nsegments ,ec_group[0].IOsegment[0],ec_group[0].IOsegment[1],ec_group[0].IOsegment[2],ec_group[0].IOsegment[3]);
-						printf("Request operational state for all slaves\n \r");
+						rt_kprintf("segments : %d : %d %d %d %d\n \r",ec_group[0].nsegments ,ec_group[0].IOsegment[0],ec_group[0].IOsegment[1],ec_group[0].IOsegment[2],ec_group[0].IOsegment[3]);
+						rt_kprintf("Request operational state for all slaves\n \r");
 						expectedWKC = (ec_group[0].outputsWKC * 2) + ec_group[0].inputsWKC;
-						printf("Calculated workcounter %d\n \r", expectedWKC);
+						rt_kprintf("Calculated workcounter %d\n \r", expectedWKC);
 						
 						/* send one valid process data to make outputs in slaves happy*/
             ec_send_processdata();
@@ -465,44 +493,58 @@ void ecat_init(void)
             
             ec_writestate(0);
             
-						printf("DC capable : %d\r\n",ec_configdc());
+						rt_kprintf("DC capable : %d\r\n",ec_configdc());
             
-            HAL_Delay(100);
+            _dm9000_delay_ms(100);
             
 					/* wait for all slaves to reach OP state */
-						do{
-							ec_slave[0].state = EC_STATE_OPERATIONAL;
-							ec_slave[1].state = EC_STATE_OPERATIONAL;
-              ec_writestate(0);
-							ec_writestate(1);
-							printf("-%d %d-",ec_slave[0].state,ec_slave[1].state);
-							}
-						while ( (ec_slave[0].state != EC_STATE_OPERATIONAL)||(ec_slave[1].state != EC_STATE_OPERATIONAL));
-//						while  (ec_slave[0].state != EC_STATE_OPERATIONAL);
-						printf("\r\n%d %d\r\n",ec_slave[0].state,ec_slave[1].state);
+						// do{
+						// 	ec_slave[0].state = EC_STATE_OPERATIONAL;
+						// 	ec_slave[1].state = EC_STATE_OPERATIONAL;
+            //   ec_writestate(0);
+						// 	ec_writestate(1);
+						// 	rt_kprintf("-%d %d-",ec_slave[0].state,ec_slave[1].state);
+						// 	}
+						// while ( (ec_slave[0].state != EC_STATE_OPERATIONAL)||(ec_slave[1].state != EC_STATE_OPERATIONAL));
+
+            ec_slave[0].state = EC_STATE_OPERATIONAL;
+            ec_send_processdata();
+            ec_receive_processdata(EC_TIMEOUTRET);
+            ec_writestate(0);
+            int chk = 200;
+            do
+            {
+              ec_send_processdata();
+              ec_receive_processdata(EC_TIMEOUTRET);
+              ec_statecheck(0, EC_STATE_OPERATIONAL, EC_TIMEOUTRET);
+            } while (chk -- && ec_slave[0].state != EC_STATE_OPERATIONAL);
+						rt_kprintf("\r\n%d %d\r\n",ec_slave[0].state,ec_slave[1].state);
+
             if (ec_slave[0].state == EC_STATE_OPERATIONAL )
             {
-                
-                outputs1 = (PDO_Output *)ec_slave[1].outputs;
-                inputs1  = (PDO_Input *)ec_slave[1].inputs;
+                for(slc = 1; slc <= ec_slavecount; slc++)
+                {
+                  outputs[slc] = (PDO_Output *)ec_slave[slc].outputs;
+                  inputs[slc]  = (PDO_Input *)ec_slave[slc].inputs;
+                }
                 dorun = 1;
 
-                printf("all slaves reached operational state.\r\n");
+                rt_kprintf("all slaves reached operational state.\r\n");
             }
 			
 							else
 						{
-							printf("Not all slaves reached operational state.\n \r");
+							rt_kprintf("Not all slaves reached operational state.\n \r");
 						}
 		}
 		else
 		{
-			printf("No slaves found!\r\n");
+			rt_kprintf("No slaves found!\r\n");
 		}
 	}
 	else
 	{
-		printf("No socket connection Excecute as root\r\n");
+		rt_kprintf("No socket connection Excecute as root\r\n");
 	}
     
 }
@@ -514,68 +556,224 @@ void ecat_init(void)
 uint8 wkc;
 int32_t start_pos = 0;
 int32_t step_increment = 10;
+int32_t stop_flag = 0;
 //1ms isr
 //int nihao = 1;
-void ecat_loop(void)
-{
-  static uint32 i = 0;
-	static uint8 flag=0;
+// void ecat_loop(void)
+// {
+//   static uint32 i = 0;
+// 	static uint8 flag=0;
 
 	
-    if (dorun>0)
-    {
-				wkc=ec_receive_processdata(EC_TIMEOUTRET);
-			//printf("%d ",wkc);
-			cur_status = inputs1->StatusWord;//0x6041
+//     if (dorun>0)
+//     {
+// 				wkc=ec_receive_processdata(EC_TIMEOUTRET);
+// 			//rt_kprintf("%d ",wkc);
+// 			cur_status = inputs[1]->StatusWord;//0x6041
 			
-			switch(startup_step)
-			{
-				case 1:
-				  outputs1->ControlWord = 0x06;//0x6040
-				  if((cur_status==0x0631)||(cur_status==0x0231)||(cur_status==0x0221)||(cur_status==0x1221))
-						 startup_step=2;
-//				  printf("0x06\n");
-					break;
-			  case 2:
-					outputs1->ControlWord = 0x07;
-					if((cur_status==0x633)||(cur_status==0x0233)||(cur_status==0x0223)||(cur_status==0x1223)||(cur_status==0x1233))
-						 startup_step=3;
-					start_pos = inputs1->CurrentPosition;
-					outputs1->TargetPos = start_pos;
-//					printf("0x07\n");
-					break;
-				case 3:
-					outputs1->ControlWord = 0x0f;
-					if((cur_status==0x1637)||(cur_status==0x1633)||(cur_status==0x1237))
-				 	//if((cur_status==0x1637)||(cur_status==0x1633))
-						 startup_step=4;
-//						 printf("0x0f\n");
-					break;
-				case 4:
-					 outputs1->ControlWord = 0x1f;	
-					 outputs1->TargetPos += step_increment;
-					break;
-				default :
-					startup_step=1;
-				//   outputs1->ControlWord = 0x03;//0x6040
-					outputs1->ControlWord = 0x80;//0x6040
-				break;
-			}		
-	 outputs1->TargetMode = 0x8;
-	 ec_send_processdata();	
-		}
+// 			switch(startup_step)
+// 			{
+// 				case 1:
+//           for(int i=1; i<= ec_slavecount; i++)
+//           {
+//             outputs[i]->ControlWord = 0x06;
+//           }
+// 				  if((cur_status==0x0631)||(cur_status==0x0231)||(cur_status==0x0221)||(cur_status==0x1221))
+// 						 startup_step=2;
+// //				  rt_kprintf("0x06\n");
+// 					break;
+// 			  case 2:
+// 					for(int i=1; i<= ec_slavecount; i++)
+//           {
+//             outputs[i]->ControlWord = 0x07;
+//           }
+// 					if((cur_status==0x633)||(cur_status==0x0233)||(cur_status==0x0223)||(cur_status==0x1223)||(cur_status==0x1233))
+// 						 startup_step=3;
+          
+//           for(int i=1; i<= ec_slavecount; i++)
+//           {
+//             outputs[i]->TargetPos = inputs[i]->CurrentPosition;
+//           }
 
+// //					rt_kprintf("0x07\n");
+// 					break;
+// 				case 3:
+// 					for(int i=1; i<= ec_slavecount; i++)
+//           {
+//             outputs[i]->ControlWord = 0x0f;
+//           }
+// 					if((cur_status==0x1637)||(cur_status==0x1633)||(cur_status==0x1237))
+// 				 	//if((cur_status==0x1637)||(cur_status==0x1633))
+// 						 startup_step=4;
+// //						 rt_kprintf("0x0f\n");
+// 					break;
+// 				case 4:
+//           for(int i=1; i<= ec_slavecount; i++)
+//           {
+//             outputs[i]->ControlWord = 0x1f;
+//             outputs[i]->TargetPos += step_increment;
+//           }
+// 					break;
+// 				default :
+// 					startup_step=1;
+// 				//   outputs1->ControlWord = 0x03;//0x6040
+// 					// outputs1->ControlWord = 0x80;//0x6040
+//           for(int i=1; i<= ec_slavecount; i++)
+//           {
+//             outputs[i]->TargetMode = 0x80;
+//           }
+// 				break;
+// 			}		
+//   for(int i=1; i<= ec_slavecount; i++)
+//   {
+//     outputs[i]->TargetMode = 0x8;
+//   }
+// 	 ec_send_processdata();	
+// 		}
+
+// }
+
+void ecat_loop(void)
+{
+  static int statup_delay_cnt = 0;
+    if (dorun > 0)
+    {
+        wkc = ec_receive_processdata(EC_TIMEOUTRET);
+
+        int all_ready = 1;
+
+        switch (startup_step)
+        {
+            case 1: // 初始化 -> Ready to Switch On
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->TargetMode = 8;         // CSP模式
+                    outputs[i]->ControlWord = 0x06;     // Shutdown
+                }
+
+                if(statup_delay_cnt < 500) // 50*loop周期(1ms) = 50ms
+                {
+                  statup_delay_cnt ++;
+                  break;
+                }
+                
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    uint16_t status = inputs[i]->StatusWord;
+                    if (!(status == 0x0231 || status == 0x0631 || status == 0x0221 || status == 0x1221)) {
+                        all_ready = 0;
+                        break;
+                    }
+                }
+
+                if (all_ready)
+                {
+                  statup_delay_cnt = 0;
+                  startup_step = 2;
+                }
+                    
+                break;
+
+            case 2: // Ready to Switch On -> Switched On
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->ControlWord = 0x07;
+                    outputs[i]->TargetPos = inputs[i]->CurrentPosition;  // 初始对齐
+                }
+
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    uint16_t status = inputs[i]->StatusWord;
+                    if (!(status == 0x0233 || status == 0x0633 || status == 0x0223 || status == 0x1223 || status == 0x1233)) {
+                        all_ready = 0;
+                        break;
+                    }
+                }
+
+                if (all_ready)
+                    startup_step = 3;
+                break;
+
+            case 3: // Switched On -> Operation Enabled
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->ControlWord = 0x0F;
+                }
+
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    uint16_t status = inputs[i]->StatusWord;
+                    if (!(status == 0x1637 || status == 0x1633 || status == 0x1237)) {
+                        all_ready = 0;
+                        break;
+                    }
+                }
+
+                if (all_ready)
+                    startup_step = 4;
+                break;
+
+            case 4: // 正常控制阶段（位置模式）
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    // outputs[i]->ControlWord = 0x1F;  // 带立即更新位
+                    outputs[i]->TargetPos += step_increment;
+                }
+
+                if(stop_flag)
+                {
+                    startup_step = 5;
+                }
+                break;
+            case 5: // 关闭电机
+                for (int i = 1; i <= ec_slavecount; i++) {
+                  outputs[i]->ControlWord = 0x0F; // 保持 Operation Enabled
+                  outputs[i]->TargetPos = inputs[i]->CurrentPosition; // 停止在当前位置
+                }
+                startup_step = 6;
+            case 6: 
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->ControlWord = 0x07;  // Shutdown
+                }
+                startup_step = 7;
+                break;
+            case 7: 
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->ControlWord = 0x06;  // Shutdown
+                }
+                startup_step = 8;
+                break;
+            case 8: 
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->ControlWord = 0x00;  
+                }
+                startup_step = 9;
+                rt_kprintf("ecat_stop acti\r\n");
+                break;
+            case 9: 
+                if(stop_flag == 0)
+                {
+                  startup_step = 0;
+                }
+                break;
+
+            default:
+                for (int i = 1; i <= ec_slavecount; i++) {
+                    outputs[i]->ControlWord = 0x80;  // 带立即更新位
+                }
+                startup_step = 1; // 重置状态机
+                
+                break;
+        }
+
+        ec_send_processdata();
+    }
 }
-
-
 
 void ecat_test_main(void *parameter)
 {
-    rt_kprintf("SOEM (Simple Open EtherCAT Master)\nSlaveinfo\n");
     rt_hw_stm32_tim();
+    rt_kprintf("rt_hw_stm32_tim\r\n");
+
     HAL_TIM_Base_Start_IT(&htim4);
-    rt_thread_mdelay(100);
+    rt_kprintf("HAL_TIM_Base_Start_IT\r\n");
+
+    _dm9000_delay_ms(100);
     ecat_init();
+    rt_kprintf("ecat_init\r\n");
 }
 
 int ecat_start(void)
@@ -599,6 +797,37 @@ int ecat_start(void)
 }
 MSH_CMD_EXPORT(ecat_start, "ecat_start");
 
+int ecat_stop(void)
+{
+  if(dorun)
+  {
+    stop_flag = 1;
+    rt_kprintf("ecat_stop\r\n");
+  }
+  return 0;
+}
+MSH_CMD_EXPORT(ecat_stop, "ecat_stop");
+
+int ecat_continue(void)
+{
+  if(stop_flag)
+  {
+    stop_flag = 0;
+    rt_kprintf("ecat_continue\r\n");
+  }
+  return 0;
+}
+MSH_CMD_EXPORT(ecat_continue, "ecat_continue");
+
+int ecat_status(void)
+{
+  rt_kprintf("dorun:%d\r\n", dorun);
+  rt_kprintf("startup_step:%d\r\n", startup_step);
+  rt_kprintf("stop_flag:%d\r\n", stop_flag);
+  return 0;
+}
+MSH_CMD_EXPORT(ecat_status, "ecat_status");
+
 extern uint32_t dorun;
 int show_flag = 0;
 int _do_ecat_show_rdo(void)
@@ -607,13 +836,13 @@ int _do_ecat_show_rdo(void)
     while (1)
     {
         if (dorun>0){
-			rt_kprintf("state:%d; StatusWord:%x, CurrentPosition:%ld,TargetMode:%d CurrentTorque:%d CurrentSpeed:%d ServoError:%d\r\n",
-			ec_slave[1].state, inputs1->StatusWord, inputs1->CurrentPosition,inputs1->TargetMode,
-			inputs1->CurrentTorque, inputs1->CurrentSpeed, inputs1->ServoError);
+//			rt_kprintf("state:%d; StatusWord:%x, CurrentPosition:%ld,TargetMode:%d CurrentTorque:%d CurrentSpeed:%d ServoError:%d\r\n",
+//			ec_slave[1].state, inputs1->StatusWord, inputs1->CurrentPosition,inputs1->TargetMode,
+//			inputs1->CurrentTorque, inputs1->CurrentSpeed, inputs1->ServoError);
         }
 		if(show_flag == 0)
 			break;
-        rt_thread_mdelay(1);
+        _dm9000_delay_ms(1);
     }
 
     return RT_EOK;
